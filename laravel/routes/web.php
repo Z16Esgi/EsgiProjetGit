d8570f30145bb9c48f6e6fa3f7e0b9fb390e5792
Z16Esgi <zbb3948@gmail.com>
2026-04-08 10:46:06 +0200

    GIT-07 ajout des routes de connexion

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/Client/Connexion',[ClientController::class,'ConnexionClient']);
+Route::post('/Client/Connecter',[ClientController::class,'ConnecterClient']);
+Route::get('/Client/Connecter', [ClientController::class, 'AfficherAccueil']);
