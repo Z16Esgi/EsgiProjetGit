bb7389983484e01bc5809b9d7f85dc3d2eb32040
Z16Esgi <zbb3948@gmail.com>
2026-04-08 11:16:30 +0200

    GIT-09 routes responsable

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', function () {
 Route::get('/Client/Connexion',[ClientController::class,'ConnexionClient']);
 Route::post('/Client/Connecter',[ClientController::class,'ConnecterClient']);
 Route::get('/Client/Connecter', [ClientController::class, 'AfficherAccueil']);
+
+Route::get('/Responsable/Programmer',[ClientController::class,'ProgrammerSejour']);
+Route::get('/Responsable/SuivreReservation',[ClientController::class,'SuivreReservation']);
+Route::get('/Responsable/AnnulerSejour',[ClientController::class,'AnnulerSejour']);
