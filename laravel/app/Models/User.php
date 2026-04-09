a5c217c19a77c1c42f47b28bb15c3c47c46eb53a
Z16Esgi <zbb3948@gmail.com>
2026-04-09 12:55:52 +0200

    GIT-18 update user

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -2,111 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use App\Http\Controllers\ClientController;
-use PDO;
-use PDOException ;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = 'client';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'nom', 'prenom', 'age', 'email', 'tel', 'mdp',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    public $timestamps = false;
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    private static $connexion = null ; 
-
-    private function __construct(){
-        self::$connexion = new PDO('mysql:host=localhost;dbname=Safar', 'boss', 'azerty');
-    }
-
-    private static function getConnexion(){
-        if(self::$connexion == null){
-            new User();
-        }
-        return self::$connexion ;
-    }
-
-    public static function programmerSejour( $ville_dep, $ville_arr, $tarif, $numHotel, $numResp){
-        $bd = self::getConnexion() ; 
-
-        $sql = 'insert into voyages values(:ville_depart, :ville_arriver, :tarif, :num_hotel, :num_responsable)';
-        $st = $bd -> prepare($sql);
-        $st -> execute( array(
-                    ':ville_depart' => $ville_dep,
-                    ':ville_arriver' => $ville_arr,
-                    ':tarif' => $tarif,
-                    ':num_hotel' => $numHotel,
-                    ':num_responsable' => $numResp
-            )
-        );
-
-        $st -> closeCursor() ; 
-
-    }
-
-    public static function getConnexionP($email, $mdp){
-        $bd = self::getConnexion();
-
-        $sql = "SELECT nom, prenom FROM client WHERE email = :email AND mdp = :mdp";
-
-        $st = $bd->prepare($sql);
-
-        $st->execute(array(':email' => $email, ':mdp' => $mdp));
-
-        $client = $st ->fetch(PDO::FETCH_ASSOC);
-
-        $st->closeCursor();
-
-        return $client;
+    public function getAuthPassword()
+    {
+        return $this->mdp;
     }
 
     public static function enregistrerClient($nom, $prenom, $age, $email, $tel, $mdp)
     {
-        $bd = self::getConnexion();
-
-        $sql = "INSERT INTO client (nom, prenom, email, age, tel, mdp) VALUES (:nom, :prenom, :age, :email, :tel, :mdp)";
-        $st = $bd->prepare($sql);
-        $st->execute([
-            ':nom'    => $nom,
-            ':prenom' => $prenom,
-            ':email'  => $email,
-            ':age'    => $age,
-            ':tel'    => $tel,
-            ':mdp'    => $mdp,
+        return self::create([
+            'nom'    => $nom,
+            'prenom' => $prenom,
+            'age'    => $age,
+            'email'  => $email,
+            'tel'    => $tel,
+            'mdp'    => $mdp, 
         ]);
-
-        $st->closeCursor();
     }
 }
